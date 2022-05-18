478ad8943122a3bf82410216363d1f4b57423de5
penicillin0 <ayahitonakamura@gmail.com>
2022-05-18 17:56:11 +0900

    new page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { FirebaseApp, getApp } from "firebase/app";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import "../firebase/init"; // Initialize FirebaseApp
 import styles from "../styles/Home.module.css";
 
@@ -17,22 +16,7 @@ const Home: NextPage = () => {
 
       <ul>
         <li>name = {app.name}</li>
-        <li>appId = {app.options.appId}</li>
-        <li>apiKey = {app.options.apiKey}</li>
       </ul>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
---
+++ pages/team.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+import styled from "styled-components";
+
+const Team: NextPage = () => {
+  return (
+    <MainContainer>
+      <h1>Team</h1>
+      <p>This is the Team page. You can edit this page by editing the file</p>
+    </MainContainer>
+  );
+};
+
+export default Team;
+
+const MainContainer = styled.div``;
