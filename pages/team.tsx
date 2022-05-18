27155d5da45995dadd21f31c32dd314437de9e46
penicillin0 <ayahitonakamura@gmail.com>
2022-05-18 17:59:01 +0900

    add tailwind

--- pages/team.tsx
+++ pages/team.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 const Team: NextPage = () => {
   return (
     <MainContainer>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <h1>Team</h1>
       <p>This is the Team page. You can edit this page by editing the file</p>
     </MainContainer>
