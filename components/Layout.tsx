36f1466045bfbeb2ebfa5fc8a1e0022fc1601f76
penicillin0 <ayahitonakamura@gmail.com>
2022-05-25 20:29:48 +0900

    add z-index

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,7 @@ type LayoutProps = Required<{
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div>
-      <div className="overflow-hidden fixed inset-0">
+      <div className="overflow-hidden fixed inset-0 -z-50">
         <div className="absolute top-[2rem] left-[-10%] w-2/5 h-[40vw] rounded-full border-3vw border-amber-400 opacity-25"></div>
         <div className="absolute top-[22rem] left-[-10rem] w-[140vw] h-[4vw] bg-slate-300 opacity-50 rotate-[330deg]"></div>
         <div className="absolute top-[20rem] right-[-10%] w-2/5 h-[40vw] rounded-full border-3vw border-teal-400 opacity-25"></div>
