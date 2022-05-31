2aaa95f2e89e14c9488c9197f0f680cccdec55cb
penicillin0 <ayahitonakamura@gmail.com>
2022-06-01 02:03:16 +0900

    for seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Layout } from "../components/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>チーム分けツール</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="チーム分けツールです。簡単にチームを作成することができます。"
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
