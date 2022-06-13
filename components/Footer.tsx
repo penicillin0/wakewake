b18964007316b709a2767b6b308ae5f9c97a1763
penicillin0 <ayahitonakamura@gmail.com>
2022-06-13 11:07:15 +0900

    add memo for footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,7 @@
-export const Footer: React.FC = () => {
-  return <div>bbbbbbb</div>;
+import React from "react";
+
+const OriginFooter: React.FC = () => {
+  return <div>this is footer</div>;
 };
+
+export const Footer = React.memo(OriginFooter);
