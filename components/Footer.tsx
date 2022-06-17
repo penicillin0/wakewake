2cdbea5176a9bb134ba0c0ecbdf7655c6160a1cc
penicillin0 <ayahitonakamura@gmail.com>
2022-06-17 21:25:25 +0900

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const OriginFooter: React.FC = () => {
-  return <div>this is footer</div>;
+  return (
+    <div className="flex justify-center  items-center w-full h-16 text-gray-500 bg-amber-100 shadow-lg">
+      this is Footer
+    </div>
+  );
 };
 
 export const Footer = React.memo(OriginFooter);
