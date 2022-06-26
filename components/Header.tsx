f65d6087a2fbf79283a583fc3abfea122d00bffa
penicillin0 <ayahitonakamura@gmail.com>
2022-06-26 22:59:37 +0900

    add link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { IconContext } from "react-icons";
 import { FaGithub } from "react-icons/fa";
@@ -10,19 +11,23 @@ const OriginHeader: React.FC = () => {
     <div className="flex justify-center w-full h-16 bg-white shadow-md opacity-70">
       <div className="flex flex-row px-2 w-[52rem] h-full">
         <div className="flex basis-3/4 items-center">
-          <Image
-            src="/logo.svg"
-            width={LOGO_SIZE}
-            height={LOGO_SIZE}
-            alt="wakewake home"
-            className="ml-1"
-          />
+          <Link href="/team" className="cursor-pointer">
+            <Image
+              src="/logo.svg"
+              width={LOGO_SIZE}
+              height={LOGO_SIZE}
+              alt="wakewake home"
+              className="ml-1"
+            />
+          </Link>
           <span className="ml-3 text-3xl text-gray-800">wakewake</span>
         </div>
         <div className="flex basis-1/4 justify-end items-center">
-          <IconContext.Provider value={{ color: "#111", size: "2.5rem" }}>
-            <FaGithub />
-          </IconContext.Provider>
+          <a href="https://github.com/penicillin0/wakewake">
+            <IconContext.Provider value={{ color: "#111", size: "2.5rem" }}>
+              <FaGithub />
+            </IconContext.Provider>
+          </a>
         </div>
       </div>
     </div>
