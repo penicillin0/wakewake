7a520b33d65170117a17dead3d7f766ce8f2445f
penicillin0 <ayahitonakamura@gmail.com>
2022-06-28 23:25:03 +0900

    wip

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const OriginFooter: React.FC = () => {
   return (
     <div className="flex sticky top-[100vh] justify-center items-center w-full h-24 text-gray-500 bg-white  shadow-[0_-10px_15px_-3px_rgba(0,0,0,0.1)] opacity-70">
-      Copyright © 2022 wakewake All Rights Reserved.
+      <span className="flex justify-center items-center w-[20rem] sm:w-[32rem] md:w-[40rem] lg:w-[52rem]">
+        Copyright © 2022 wakewake All Rights Reserved.
+      </span>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -191,7 +191,7 @@ const Home: NextPage = () => {
 
   return (
     <div className="flex justify-center" id="capture">
-      <div className="my-8 w-[52rem]">
+      <div className="my-8 w-[20rem] sm:w-[32rem] md:w-[40rem] lg:w-[52rem]">
         <div>
           <div className="mt-8 border-l-4 border-gray-500 border-solid">
             <h1 className="ml-2 text-xl">使い方</h1>
