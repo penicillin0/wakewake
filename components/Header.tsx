bf4a3e218f329fb5ad3701aded850d9663893f5e
penicillin0 <ayahitonakamura@gmail.com>
2022-07-01 11:45:20 +0900

    fix design

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,23 +4,23 @@ import { IconContext } from "react-icons";
 import { AiFillTwitterCircle } from "react-icons/ai";
 import { FaGithub } from "react-icons/fa";
 
-const LOGO_SIZE = "52";
+const LOGO_SIZE = "52px";
 
 const OriginHeader: React.FC = () => {
   return (
-    <div className="flex justify-center w-full h-16 bg-white shadow-md opacity-70">
-      <div className="flex flex-row px-2 w-[20rem] h-full sm:w-[32rem] md:w-[40rem] lg:w-[52rem]">
+    <div className="flex justify-center w-full h-[64px] bg-white shadow-md opacity-70">
+      <div className="flex flex-row px-[8px] w-full h-full sm:w-[512px] md:w-[640px] lg:w-[832px]">
         <div className="flex basis-3/4 items-center">
           <Image
             src="/logo.svg"
             width={LOGO_SIZE}
             height={LOGO_SIZE}
             alt="wakewake home"
-            className="ml-1"
+            className="ml-[4px]"
           />
           <span className="ml-3 text-3xl text-gray-800">wakewake</span>
         </div>
-        <div className="flex basis-1/4 gap-x-1 justify-end items-center">
+        <div className="flex basis-1/4 gap-x-[4px] justify-end items-center">
           <a href="https://github.com/penicillin0/wakewake">
             <IconContext.Provider value={{ color: "#111", size: "42px" }}>
               <FaGithub title="Github Link" />
