78a814e7086ced9770626053db3983a7eef11b07
penicillin0 <ayahitonakamura@gmail.com>
2022-07-11 12:29:22 +0900

    tweak

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const OriginFooter: React.FC = () => {
   return (
     <div className="flex sticky top-[100vh] justify-center items-center w-full h-[96px] text-gray-500 bg-white  shadow-[0_-10px_15px_-3px_rgba(0,0,0,0.1)] opacity-70">
-      <span className="flex justify-center items-center w-[340px] sm:w-[512px] md:w-[640px] lg:w-[832px]">
+      <span className="flex justify-center items-center w-[340px] text-[10px] sm:w-[512px] md:w-[640px] lg:w-[832px]">
         Copyright © 2022 wakewake All Rights Reserved.
       </span>
     </div>
